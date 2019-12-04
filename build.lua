1358c64524d4424c6e51928ffa081196dc79c654
David Carlisle <d.p.carlisle@gmail.com>
2019-12-04 18:59:41 +0000

    initial setup

---
+++ build.lua
@@ -0,0 +1,63 @@
+#!/usr/bin/env texlua
+
+
+module = "accsupp"
+
+installfiles = {
+'*.sty',
+'*.dtx','*.pdf',
+'*.def'
+}
+
+
+textfiles = {"README.md"}
+unpackfiles = {'accsupp.dtx'}
+
+packtdszip  = true
+
+maxprintline=10000
+checkruns = 2
+
+tdslocations={
+"doc/latex/accsupp/accsupp-example1.tex",
+"doc/latex/accsupp/accsupp-example2.tex",
+"doc/latex/accsupp/accsupp.pdf",
+"source/latex/accsupp/accsupp.dtx",
+"tex/latex/accsupp/accsupp-dvipdfm.def",
+"tex/latex/accsupp/accsupp-dvips.def",
+"tex/latex/accsupp/accsupp-luatex.def",
+"tex/latex/accsupp/accsupp-pdftex.def",
+"tex/latex/accsupp/accsupp.sty",
+}
+
+function update_tag(file,content,tagname,tagdate)
+
+local tagpattern="(%d%d%d%d[-/]%d%d[-/]%d%d) v(%d+[.])(%d+)"
+local oldv,newv
+if tagname == 'auto' then
+  local i,j,olddate,a,b
+  i,j,olddate,a,b= string.find(content, tagpattern)
+  if i == nil then
+    print('OLD TAG NOT FOUND')
+    return content
+  else
+    print ('FOUND: ' .. olddate .. ' v' .. a .. b )
+    oldv = olddate .. ' v' .. a .. b
+    newv = tagdate .. ' v'  .. a .. math.floor(b + 1)
+    print('USING OLD TAG: ' .. oldv)
+    print('USING NEW TAG: ' .. newv)
+    content=string.gsub(content,"{Version}{" .. oldv,'##OLDV##')
+    content=string.gsub(content,oldv,newv)
+    content=string.gsub(content,'##OLDV##',"{Version}{" .. oldv)
+    content = string.gsub(content,
+        '%% \\end{History}',
+	'%%   \\begin{Version}{' .. newv .. '}\n%%   \\item Updated\n%%   \\end{Version}\n%% \\end{History}')
+    return content
+  end
+else
+  error("only automatic tagging supported")
+end
+
+end
+
+
